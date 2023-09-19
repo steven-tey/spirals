8a3b316006c3abb0d5e1fa91e9763ec260b2387e
Steven Tey <stevensteel97@gmail.com>
2023-09-19 11:58:48 -0700

    added dynamic og

--- app/t/[id]/page.tsx
+++ app/t/[id]/page.tsx
@@ -1,6 +1,45 @@
 import { kv } from "@vercel/kv";
 import { notFound } from "next/navigation";
 import Body from "@/components/body";
+import { Metadata } from "next";
+
+export async function generateMetadata({
+  params,
+}: {
+  params: {
+    id: string;
+  };
+}): Promise<Metadata | undefined> {
+  const data = await kv.hgetall<{ prompt: string; image?: string }>(params.id);
+  if (!data) {
+    return;
+  }
+
+  const title = `Spirals: ${data.prompt}`;
+  const description = `A spiral generated from the prompt: ${data.prompt}`;
+  const image = data.image || "https://spirals.vercel.app/opengraph-image.png";
+
+  return {
+    title,
+    description,
+    openGraph: {
+      title,
+      description,
+      images: [
+        {
+          url: image,
+        },
+      ],
+    },
+    twitter: {
+      card: "summary_large_image",
+      title,
+      description,
+      images: [image],
+      creator: "@steventey",
+    },
+  };
+}
 
 export default async function Results({
   params,
