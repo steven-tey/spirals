5df74581fb9df6032c96a6f9533ab3959cd33abb
Steven Tey <stevensteel97@gmail.com>
2023-09-19 11:43:43 -0700

    add WEBHOOK_URL variable

--- lib/actions.ts
+++ lib/actions.ts
@@ -3,6 +3,7 @@
 import Replicate from "replicate";
 import { kv } from "@vercel/kv";
 import { nanoid } from "./utils";
+import { WEBHOOK_URL } from "./constants";
 
 const replicate = new Replicate({
   auth: process.env.REPLICATE_API_TOKEN as string,
@@ -25,9 +26,10 @@ export async function generate(form: FormData) {
         qr_code_content: "https://spirals.vercel.app",
         image:
           "https://xd2kcvzsdpeyx1gu.public.blob.vercel-storage.com/8uiaWqu-77Maq6Zn38dfz9iWwXsyaheFfOSJPL.png",
-        controlnet_conditioning_scale: 1.5,
+        controlnet_conditioning_scale: 1,
+        qrcode_background: "white",
       },
-      webhook: `https://46a3-2600-1700-2f70-6600-cd58-6fd9-b8f4-e48.ngrok-free.app/api/webhook?id=${id}`,
+      webhook: `${WEBHOOK_URL}?id=${id}`,
       webhook_events_filter: ["completed"],
     }),
   ]);
