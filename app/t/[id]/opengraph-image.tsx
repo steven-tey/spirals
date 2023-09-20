70c86af56f5836a22a460c31489a4fcf54c5e703
Steven Tey <stevensteel97@gmail.com>
2023-09-19 17:17:40 -0700

    tweak OG

--- app/t/[id]/opengraph-image.tsx
+++ app/t/[id]/opengraph-image.tsx
@@ -36,11 +36,11 @@ export default async function DynamicOG({
           tw="absolute flex bottom-[48px] flex items-center basis-full lg:basis-auto"
           style={{ gap: 28 }}
         >
-          <div tw="bg-gray-200 border-4 border-black px-[52px] lowercase break-all rounded-lg text-[36px] flex items-center justify-start w-[800px] h-[150px]">
+          <div tw="bg-white border-4 border-black px-[52px] lowercase break-all rounded-lg text-[36px] flex items-center justify-start w-[800px] h-[150px]">
             {data.prompt.substring(0, 72)}
             {data.prompt.length > 72 && "..."}
           </div>
-          <div tw="flex bg-gray-200 border-4 border-black p-5 rounded-lg relative">
+          <div tw="flex bg-white border-4 border-black p-5 rounded-lg relative">
             <svg
               version="1.0"
               xmlns="http://www.w3.org/2000/svg"
