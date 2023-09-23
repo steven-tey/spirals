781d58072e28c5f006986220b445e9f9a8375929
Steven Tey <stevensteel97@gmail.com>
2023-09-23 13:01:15 -0700

    added copy image button

--- components/photo-booth.tsx
+++ components/photo-booth.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Download } from "lucide-react";
+import { Copy, Download } from "lucide-react";
 import Image from "next/image";
 import { useEffect, useState } from "react";
 import { LoadingCircle } from "@/components/icons";
@@ -16,16 +16,11 @@ function forceDownload(blobUrl: string, filename: string) {
   a.remove();
 }
 
-export default function PhotoBooth({
-  image,
-  failed,
-}: {
-  image: string | null;
-  failed?: boolean;
-}) {
+export default function PhotoBooth({ image }: { image: string | null }) {
   const router = useRouter();
   const params = useParams();
   const { id } = params;
+  const [copying, setCopying] = useState(false);
   const [downloading, setDownloading] = useState(false);
 
   useEffect(() => {
@@ -45,44 +40,81 @@ export default function PhotoBooth({
       className="group relative mx-auto mt-6 aspect-square w-full max-w-xl animate-fade-up overflow-hidden rounded-2xl border border-gray-200 opacity-0"
       style={{ animationDelay: "0.4s", animationFillMode: "forwards" }}
     >
-      {id && image && !failed && (
-        <button
-          onClick={() => {
-            setDownloading(true);
-            va.track("download image", {
-              image,
-              page: `https://spirals.vercel.app/t/${id}`,
-            });
-            fetch(image, {
-              headers: new Headers({
-                Origin: location.origin,
-              }),
-              mode: "cors",
-            })
-              .then((response) => response.blob())
-              .then((blob) => {
-                let blobUrl = window.URL.createObjectURL(blob);
-                forceDownload(blobUrl, `${id || "demo"}.png`);
-                setDownloading(false);
+      {id && image && (
+        <div className="absolute right-5 top-5 z-10 flex space-x-2">
+          <button
+            onClick={() => {
+              setCopying(true);
+              va.track("copy image", {
+                image,
+                page: `https://spirals.vercel.app/t/${id}`,
+              });
+              fetch(image, {
+                headers: new Headers({
+                  Origin: location.origin,
+                }),
+                mode: "cors",
               })
-              .catch((e) => console.error(e));
-          }}
-          className="absolute right-5 top-5 z-10 flex h-9 w-9 items-center justify-center rounded-full border border-gray-200 bg-white shadow-sm transition-all hover:scale-105 active:scale-95"
-        >
-          {downloading ? (
-            <LoadingCircle />
-          ) : (
-            <Download className="h-5 w-5 text-gray-500" />
-          )}
-        </button>
-      )}
-      {failed ? (
-        <div className="z-10 flex h-full w-full flex-col items-center bg-white pt-[140px] sm:pt-[280px]">
-          <p className="text-sm text-red-600">
-            Failed to run – please try again!
-          </p>
+                .then((response) => response.blob())
+                .then((blob) => {
+                  let blobUrl = window.URL.createObjectURL(blob);
+                  navigator.clipboard.write([
+                    new ClipboardItem({
+                      "image/png": blob,
+                    }),
+                  ]);
+                  setCopying(false);
+                })
+                .catch((e) => console.error(e));
+            }}
+            className="flex h-9 w-9 items-center justify-center rounded-full border border-gray-200 bg-white shadow-sm transition-all hover:scale-105 active:scale-95"
+          >
+            {copying ? (
+              <LoadingCircle />
+            ) : (
+              <Copy className="h-4 w-4 text-gray-500" />
+            )}
+          </button>
+          <button
+            onClick={() => {
+              setDownloading(true);
+              va.track("download image", {
+                image,
+                page: `https://spirals.vercel.app/t/${id}`,
+              });
+              fetch(image, {
+                headers: new Headers({
+                  Origin: location.origin,
+                }),
+                mode: "cors",
+              })
+                .then((response) => response.blob())
+                .then((blob) => {
+                  let blobUrl = window.URL.createObjectURL(blob);
+                  forceDownload(blobUrl, `${id || "demo"}.png`);
+                  setDownloading(false);
+                })
+                .catch((e) => console.error(e));
+            }}
+            className="flex h-9 w-9 items-center justify-center rounded-full border border-gray-200 bg-white shadow-sm transition-all hover:scale-105 active:scale-95"
+          >
+            {downloading ? (
+              <LoadingCircle />
+            ) : (
+              <Download className="h-4 w-4 text-gray-500" />
+            )}
+          </button>
         </div>
-      ) : !image ? (
+      )}
+      {image ? (
+        <Image
+          alt="output image"
+          src={image}
+          width={1280}
+          height={1280}
+          className="h-full object-cover"
+        />
+      ) : (
         <div className="z-10 flex h-full w-full flex-col items-center bg-white pt-[140px] sm:pt-[280px]">
           <LoadingCircle />
           {id && (
@@ -96,14 +128,6 @@ export default function PhotoBooth({
             </div>
           )}
         </div>
-      ) : (
-        <Image
-          alt="output image"
-          src={image}
-          width={1280}
-          height={1280}
-          className="h-full object-cover"
-        />
       )}
     </div>
   );
